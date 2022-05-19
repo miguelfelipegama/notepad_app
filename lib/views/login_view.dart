8ecae962e83bd11b8d92bae24f1b828e790d34cc
Miguel Felipe da Gama Melo <fiscaltodahora@gmail.com>
2022-05-19 16:26:04 -0300

    Creates login and handles auth exceptions

--- lib/views/login_view.dart
+++ lib/views/login_view.dart
@@ -1,28 +1,16 @@
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:firebase_auth/firebase_auth.dart';
-import 'package:notepad_app/views/login_view.dart';
-import 'firebase_options.dart';
+import 'package:notepad_app/firebase_options.dart';
 
-void main() {
-  WidgetsFlutterBinding.ensureInitialized();
-  runApp(MaterialApp(
-    title: 'Flutter Demo',
-    theme: ThemeData(
-      primarySwatch: Colors.blue,
-    ),
-    home: const RegisterView(),
-  ));
-}
-
-class RegisterView extends StatefulWidget {
-  const RegisterView({Key? key}) : super(key: key);
+class LoginView extends StatefulWidget {
+  const LoginView({Key? key}) : super(key: key);
 
   @override
-  State<RegisterView> createState() => _RegisterViewState();
+  State<LoginView> createState() => _LoginViewState();
 }
 
-class _RegisterViewState extends State<RegisterView> {
+class _LoginViewState extends State<LoginView> {
   late final TextEditingController _email;
   late final TextEditingController _password;
 
@@ -44,7 +32,7 @@ class _RegisterViewState extends State<RegisterView> {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: const Text('Register'),
+        title: const Text('Login'),
       ),
       body: FutureBuilder(
           future: Firebase.initializeApp(
@@ -68,19 +56,19 @@ class _RegisterViewState extends State<RegisterView> {
                   autocorrect: false,
                 ),
                 TextButton(
-                  child: const Text('Register'),
+                  child: const Text('Login'),
                   onPressed: () async {
                     final email = _email.text;
                     final password = _password.text;
                     try {
                       final userCredential = await FirebaseAuth.instance
-                          .createUserWithEmailAndPassword(
+                          .signInWithEmailAndPassword(
                               email: email, password: password);
                     } on FirebaseAuthException catch (e) {
                       showDialog<String>(
                         context: context,
                         builder: (BuildContext context) => AlertDialog(
-                          title: const Text('Registration Error'),
+                          title: const Text('Login Error'),
                           content: Text(e.code.toString()),
                           actions: <Widget>[
                             TextButton(
